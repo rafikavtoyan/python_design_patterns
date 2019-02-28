3dcb40c09959b8bb67083036479d9ef2352f01ae
Rafik Avtoyan <rafik.avtoyan@betconstruct.com>
2019-02-28 13:32:29 +0400

    Add python and observer patterns.

---
+++ command/command_stock_exchange.py
@@ -0,0 +1,51 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Order(metaclass=ABCMeta):
+    @abstractmethod
+    def execute(self):
+        pass
+
+
+class BuyStockOrder(Order):
+    def __init__(self, stock):
+        self.stock = stock
+
+    def execute(self):
+        self.stock.buy()
+
+
+class SellStockOrder(Order):
+    def __init__(self, stock):
+        self.stock = stock
+
+    def execute(self):
+        self.stock.sell()
+
+
+class StockTrade:
+    def buy(self):
+        print("You will buy stocks")
+
+    def sell(self):
+        print("You will sell stocks")
+
+
+class Agent:
+    def __init__(self):
+        self.__orderQueue = []
+
+    def place_order(self, order):
+        self.__orderQueue.append(order)
+        order.execute()
+
+
+if __name__ == '__main__':  # Client
+    stock = StockTrade()
+    buyStock = BuyStockOrder(stock)
+    sellStock = SellStockOrder(stock)
+
+    # Invoker
+    agent = Agent()
+    agent.place_order(buyStock)
+    agent.place_order(sellStock)
